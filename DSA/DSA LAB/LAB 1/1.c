c790535adf8925f0a3d56bc0224d70bf3bc2c205
TANMAY0704 <PANDEY.TANMAY17@GMAIL.COM>
2022-10-30 13:58:58 +0530

    hu

--- DSA/DSA LAB/LAB 1/1.c
+++ DSA/DSA LAB/LAB 1/1.c
@@ -2,27 +2,22 @@
 
 struct emp
 {
-   int id;
-   char name[20];
-   int age;
-   int salary;
+    int id;
+    char name[20];
+    int age;
+    int salary;
 };
 int main()
 {
-    emp e;
+    struct emp e;
     printf("enter id");
-    scanf("%d",&e.id);
+    scanf("%d", &e.id);
     printf("enter name");
-    scanf("%s",e.name);
+    scanf("%s", e.name);
     printf("enter age");
-    scanf("%d",&e.age);
+    scanf("%d", &e.age);
     printf("enter salary");
-    scanf("%d",&e.salary);
-    
-    printf("emp id is %d \n emp name is %s\n emp age is %d\n emp salary is %d\n",e.id,e.name,e.age,e.salary);
-    
-    
+    scanf("%d", &e.salary);
 
-
-    
+    printf("emp id is %d \n emp name is %s\n emp age is %d\n emp salary is %d\n", e.id, e.name, e.age, e.salary);
 }
