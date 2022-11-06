438f3398c3cbd265003775e051957d7e27660e41
Tanmay Pandey <PANDEY.TANMAY17@GMAIL.COM>
2022-11-06 22:03:50 +0530

    gi

--- DSA/praca/dll.c
+++ DSA/praca/dll.c
@@ -1,1 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next, *prev;
+};
+struct node *start, *nnode, *curr;
+void create()
+{
+    int i, n;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        nnode = (struct node *)malloc(sizeof(struct node));
+        printf("Enter the data: ");
+        scanf("%d", &nnode->data);
+        nnode->next = NULL;
+        nnode->prev = NULL;
+        if (start == NULL)
+        {
+            start = nnode;
+            curr = nnode;
+        }
+        else
+        {
+            curr->next = nnode;
+            nnode->prev = curr;
+            curr = nnode;
+        }
+    }
+}
+void display()
+{
+    curr = start;
+    printf("The list is: ");
+    while (curr != NULL)
+    {
+        printf("%d ", curr->data);
+        curr = curr->next;
+    }
+    printf("\n");
+}
+void insertion()
+{
+    int s;
+    nnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the data: ");
+    scanf("%d", &nnode->data);
+    printf("At start press 1, at end press 2, at any other position press 3: ");
+    printf("Enter choice : ");
+    scanf("%d", &s);
+    if (s == 1)
+    {
+        nnode->next = start;
+        start->prev = nnode;
+        start = nnode;
+    }
+    else if (s == 2)
+    {
+        curr = start;
+        while (curr->next != NULL)
+        {
+            curr = curr->next;
+        }
+        curr->next = nnode;
+        nnode->prev = curr;
+    }
+    else if (s == 3)
+    {
+        int pos, i;
+        printf("Enter the position: ");
+        scanf("%d", &pos);
+        curr = start;
+        for (i = 1; i < pos; i++)
+        {
+            curr = curr->next;
+        }
+        nnode->next = curr->next;
+        curr->next->prev = nnode;
+        curr->next = nnode;
+        nnode->prev = curr;
+    }
+}
+void deletion()
+{
+    int s;
+    printf("At start press 1, at end press 2, at any other position press 3: ");
+    printf("Enter choice : ");
+    scanf("%d", &s);
+    if (s == 1)
+    {
+        curr = start;
+        start = start->next;
+        start->prev = NULL;
+        free(curr);
+    }
+    else if (s == 2)
+    {
+        curr = start;
+        while (curr->next != NULL)
+        {
+            curr = curr->next;
+        }
+        curr->prev->next = NULL;
+        free(curr);
+    }
+    else if (s == 3)
+    {
+        int pos, i;
+        printf("Enter the position: ");
+        scanf("%d", &pos);
+        curr = start;
+        for (i = 1; i < pos; i++)
+        {
+            curr = curr->next;
+        }
+        curr->prev->next = curr->next;
+        curr->next->prev = curr->prev;
+        free(curr);
+    }
+}
 
+int main()
+{
+    create();
+    display();
+    insertion();
+    display();
+    deletion();
+    display();
+    return 0;
+}
--- DSA/praca/ll.c
+++ DSA/praca/ll.c
@@ -5,144 +5,63 @@ struct node
     int data;
     struct node *next;
 };
-struct node *start;
-void create(struct node *start)
+struct node *start, *nnode, *curr;
+void create()
 {
-    struct node *curr;
-    int a;
-
-    curr = start;
-    printf("Enter data : ");
-    scanf("%d", &curr->data);
-    printf("Press 1 to continue : ");
-    scanf("%d", &a);
-    curr->next = NULL;
-    while (a == 1)
+    int i, n;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
     {
-        curr = (struct node *)malloc(sizeof(struct node));
-        printf("Enter data : ");
-        scanf("%d", &curr->data);
-        start = curr;
-        start->next = curr;
-        printf("Press 1 to continue : ");
-        scanf("%d", &a);
+        nnode = (struct node *)malloc(sizeof(struct node));
+        printf("Enter the data: ");
+        scanf("%d", &nnode->data);
+        nnode->next = NULL;
+        if (start == NULL)
+        {
+            start = nnode;
+            curr = nnode;
+        }
+        else
+        {
+            curr->next = nnode;
+            curr = nnode;
+        }
     }
-    curr->next = NULL;
 }
 void display()
 {
-    struct node *curr;
     curr = start;
-    while (curr != NULL)
+    printf("The list is: ");
+    while (curr!= NULL)
     {
         printf("%d ", curr->data);
         curr = curr->next;
     }
     printf("\n");
 }
-void insert()
+void insertion()
 {
-    struct node *nnode, *curr;
-    nnode = (struct node *)malloc(sizeof(struct node));
-    int choice;
-    printf("\n1.At Start\n2.At End\n3.At Specific location\nEnter choice : ");
-    scanf("%d", &choice);
-    if (choice == 1)
-    {
-<<<<<<< Updated upstream
-        printf("Enter data of new node : ");
-=======
-        printf("Enter data of new nod : ");
->>>>>>> Stashed changes
-        scanf("%d", &nnode->data);
-        nnode->next = start;
-        nnode = start;
-    }
-    else if (choice == 2)
-    {
-        curr = start;
-        printf("Enter data of new node : ");
-        scanf("%d", &nnode->data);
-        while (curr->next != NULL)
-        {
-            curr = curr->next;
-        }
-        curr->next = nnode;
-        nnode->next = NULL;
-    }
-    else
-    {
-        curr = start;
-        int s;
-        printf("Enter loaction : ");
-        scanf("%d", &s);
-        printf("Enter data of new node : ");
-        scanf("%d", &nnode->data);
-        for (int i = 1; i < s; i++)
-        {
-            curr = curr->next;
-        }
-        nnode->next = curr->next;
-        curr->next = nnode;
-    }
-}
-void delete()
-{
-    struct node *temp, *curr;
-    int choice;
-    curr = start;
-    printf("\n1.At Start\n2.At End\n3.At Specific location\nEnter choice : ");
-    scanf("%d", &choice);
-    if (choice == 1)
-    {
-        start = start->next;
-        free(curr);
-    }
-    else if (choice == 2)
-    {
 
-        while (curr->next != NULL)
-        {
-            temp = curr;
-            curr = curr->next;
-        }
-        temp->next = NULL;
-        free(curr);
-    }
-    else
-    {
-        int s;
-        printf("Enter loaction : ");
-        scanf("%d", &s);
-        for (int i = 1; i < s - 1; i++)
-        {
-            curr = curr->next;
-        }
-        curr->next = curr->next->next;
-    }
+    nnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the data: ");
+    scanf("%d", &nnode->data);
+    nnode->next = start;
+    start = nnode;
+}
+void deletion()
+{   
+    curr = start;
+    start = start->next;
+    free(curr);
 }
 int main()
 {
-    struct node *a;
-    a = (struct node *)malloc(sizeof(struct node));
-    int x;
-    create(a);
-f:
-    printf("\n1.Display\n2.Insertion\n3.Deletion\nEnter Choice : ");
-    scanf("%d", &x);
-    switch (x)
-    {
-    case 1:
-        display();
-        goto f;
-    case 2:
-        insert();
-        display();
-        goto f;
-    case 3:
-        delete();
-        display();
-        goto f;
-    }
+    create();
+    display();
+    insertion();
+    display();
+    deletion();
+    display();
     return 0;
 }
