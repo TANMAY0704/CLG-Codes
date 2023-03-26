9b32f73753f310c8938c2fecf4035f9f4d974256
TANMAY0704 <PANDEY.TANMAY17@GMAIL.COM>
2023-03-26 21:55:08 +0530

    hi

---
+++ OS_LAB/LAB8/sjf.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct process
+{
+    int pno;
+    int at, bt, ct, tat, rt, wt;
+};
+
+int main()
+{
+    int n, i, j;
+    printf("Enter the number of processes : ");
+    scanf("%d", &n);
+    struct process p[n];
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the arrival time and burst time of process %d : ", i + 1);
+        scanf("%d%d", &p[i].at, &p[i].bt);
+        p[i].pno = i + 1;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (p[i].bt > p[j].bt)
+            {
+                struct process temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+
+    int time = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (time < p[i].at)
+        {
+            time = p[i].at;
+        }
+        time += p[i].bt;
+        p[i].ct = time;
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+        p[i].rt = p[i].wt;
+    }
+
+    printf("Process\tAT\tBT\tCT\tTAT\tWT\tRT \n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].pno, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt, p[i].rt);
+    }
+}
